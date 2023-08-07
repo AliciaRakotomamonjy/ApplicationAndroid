07439915cddb5977973d82a3fc8a23cb17cc6a9f
laetitiarabibizaka <laetitiarabibizaka@gmail.com>
2023-08-07 23:32:41 +0300

    Revert "Revert "Merge branch 'setting-login' into features/association""
    This reverts commit de62c3ababd641ef5fd8a46c46a8aeca1624680f.

---
+++ app/src/main/java/com/example/applicationandroid/ui/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.applicationandroid.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.applicationandroid.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_layout);
+    }
+}
