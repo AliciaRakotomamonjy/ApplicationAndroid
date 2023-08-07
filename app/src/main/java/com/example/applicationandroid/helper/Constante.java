90396471634ea37402bd0d8f85bec4cf25ba5f81
AliciaRakotomamonjy <aliciarakotomamonjy@gmail.com>
2023-08-07 23:08:16 +0300

    [Constante] change the base url

--- app/src/main/java/com/example/applicationandroid/helper/Constante.java
+++ app/src/main/java/com/example/applicationandroid/helper/Constante.java
@@ -2,7 +2,7 @@ package com.example.applicationandroid.helper;
 
 public class Constante {
 
-    public static String BASE_URL = "https://a73f-154-126-56-74.ngrok-free.app";
+    public static String BASE_URL = "https://manakory-api.onrender.com";
     public static String API_URL = BASE_URL+"/api";
 
     public static String DEFAULT_IMAGE = "image_par_defaut";
